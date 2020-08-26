62c0a0f892eb25ec1fd1f4a28197daba28e777d0
Jean Domingues <jeandomingues752@gmail.com>
2020-08-26 16:28:26 -0300

    Criando os detalhes do server

---
+++ src/components/ServerName/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: var(--secondary);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 11px 0 16px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
+  z-index: 2;
+`;
+
+export const Title = styled.div`
+  font-weight: bold;
+  color: var(--white);
+
+  
+`;
+export const ExpandIcon = styled.div``;
