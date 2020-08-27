27e43afc081f050085c8d7424657cd719b96457b
Jean Domingues <jeandomingues752@gmail.com>
2020-08-27 15:02:53 -0300

    Criando os detalhes do servidor

--- src/components/ChannelList/index.tsx
+++ src/components/ChannelList/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Category, AddCategoryIcon } from './styles';
+import ChannelButton from '../ChannnelButton';
 
 const ChannelList: React.FC = () => {
   return (
     <Container>
+      <Category>
+        <span>Canais de texto</span>
+        <AddCategoryIcon />
+      </Category>
 
+      <ChannelButton channelName='chat-livre' />
+      <ChannelButton channelName='rainbow six' />
+      <ChannelButton channelName='lolzinho' />
+      <ChannelButton channelName='csgo' />
+      <ChannelButton channelName='Valorant' />
     </Container>
-  )
-}
+  );
+};
 
 export default ChannelList;
