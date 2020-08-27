84baab4bdfede3a99c5ef01b8d3c07dc78652804
Jean Domingues <jeandomingues752@gmail.com>
2020-08-27 12:30:51 -0300

    Criando o componente Channelinfo

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container  } from './styles';
+
+const ChannelInfo: React.FC = () => {
+  return (
+    <Container>
+     
+
+    </Container>
+  )
+}
+
+export default ChannelInfo;
--- src/components/ServerName/styles.ts
+++ src/components/ServerName/styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
+import { ExpandMore } from 'styled-icons/material'
+
 export const Container = styled.div`
   background: var(--secondary);
 
   display: flex;
-  justify-content: space-between;
+  justify-content: space-between; 
   align-items: center;
 
   padding: 0 11px 0 16px;
@@ -12,10 +14,17 @@ export const Container = styled.div`
   z-index: 2;
 `;
 
-export const Title = styled.div`
+export const Title = styled.h1`
+  font-size: 16px;
   font-weight: bold;
   color: var(--white);
 
   
 `;
-export const ExpandIcon = styled.div``;
+export const ExpandIcon = styled(ExpandMore)`
+  width: 28px;
+  height: 28px;
+
+  color: var(--white);
+  cursor: pointer;
+`;
