0874ca4538933c67dbb68b94e5b3360caeb89ff5
Jean Domingues <jeandomingues752@gmail.com>
2020-08-30 12:11:51 -0300

    Finalizando a aplicação

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
+import { Mention } from '../ChannelMessage/styles';
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 
+import ChannelMessage from '../ChannelMessage';
+
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(()=> {
+    const div = messagesRef.current;
+
+    if(div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
   return (
     <Container>
-      <Messages />
+      <Messages ref={messagesRef}>
+        {Array.from(Array(40).keys()).map((n) => (
+          <ChannelMessage author='Jean Domingues' date='28/08/2020' content='Oi, meu nome é jean' />
+        ))}
+
+        <ChannelMessage
+          author='João Felipe Camargo'
+          date='28/08/2020'
+          content={
+            <>
+              <Mention>@Jean_Domingues</Mention> Bora joga
+            </>
+          }
+        />
+      </Messages>
 
       <InputWrapper>
-        <Input type="text" placeholder="Conversar em #chat-livre" />
+        <Input type='text' placeholder='Conversar em #chat-livre' />
         <InputIcon />
       </InputWrapper>
     </Container>
